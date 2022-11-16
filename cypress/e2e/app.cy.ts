3956ea281a879c99b2ceb6928f41962fd33fc576
MatthiasFaucon <92744941+matthiasfaucon@users.noreply.github.com>
2022-11-16 10:58:45 +0000

    delete last test and actions/checkout

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -13,9 +13,6 @@ describe('Navigation', () => {
 
     // The new url should include "/about"
     cy.url().should('include', '/about')
-
-    // The new page should contain an h1 with "About page"
-    cy.get('h1').contains('About Page')
   })
 })
 
